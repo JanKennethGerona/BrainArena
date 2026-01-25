392c3fa21e3c337d5148f4e4426a08685ceea166
JanKennethGerona <jkdgerona@addu.edu.ph>
2026-01-25 18:38:10 +0800

    idk

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,20 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
   base: '/BrainArena/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true
-  },
+  plugins: [react()],
   server: {
     port: 3000,
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false
+        changeOrigin: true
       }
     }
   }
